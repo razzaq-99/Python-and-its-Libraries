77fc8d18b0cba8a5e308842d2b182c0252600884
abdul Razzaq <ar8937562@gmail.com>
2023-12-15 23:54:21 +0500

    variables and data types in python

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("Hello World!")
-print("I am starting python language")
+# print("Hello world!!!",45,00)
+# print(22+34)
 
+# print("number ",45,566, sep=" - ")
+
+
+a = 1234
+b = "abc"
+c = True
+list1 = [1,2,3,"Ok",22.2]
+# print(a)
+# print(b)
+# print(c)
+# print(list1)
+
+
+# print(a+b)
+# print(b+c)
+
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))
+print("the type of c is ", type(c))
+print("the type of list1 is ", type(list1))
