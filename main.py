9832712085bf1aa7b5638042e136934d7ea857da
abdul Razzaq <ar8937562@gmail.com>
2023-12-16 21:35:49 +0500

    operators and calculator in python

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 # print("number ",45,566, sep=" - ")
 
 
-a = 1234
-b = "abc"
-c = True
-list1 = [1,2,3,"Ok",22.2]
+# a = 1234
+# b = "abc"
+# c = True
+# list1 = [1,2,3,"Ok",22.2]
 # print(a)
 # print(b)
 # print(c)
@@ -17,7 +17,21 @@ list1 = [1,2,3,"Ok",22.2]
 # print(a+b)
 # print(b+c)
 
-print("the type of a is ", type(a))
-print("the type of b is ", type(b))
-print("the type of c is ", type(c))
-print("the type of list1 is ", type(list1))
+# print("the type of a is ", type(a))
+# print("the type of b is ", type(b))
+# print("the type of c is ", type(c))
+# print("the type of list1 is ", type(list1))
+
+
+
+# num1 = 23
+# num2 = 45
+
+# print("the addition of ",num1 , " and ", num2 , " is " , num1+num2)
+
+# print("the subtraction of ",num1 , " and ", num2 , " is " , num1-num2)
+
+# print("the multiplication of ",num1 , " and ", num2 , " is " , num1*num2)
+
+# print("the division of ",num1 , " and ", num2 , " is " , num1/num2)
+
