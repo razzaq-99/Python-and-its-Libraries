9b1a1118e988e347d2004bbc24a6743978a9cf50
abdul Razzaq <ar8937562@gmail.com>
2023-12-19 00:07:23 +0500

    functions in Python

---
+++ functions.py
@@ -0,0 +1,81 @@
+                      
+                                    # Functions in Python
+                                    
+# def calmean(a,b):
+#       mean = (a*b)/(a+b) 
+#       print(mean)       
+                               
+
+
+# def isGreater(a,b):
+#     if(a>b):
+#      print("First number is greater")
+#     elif(a==b):
+#         print("Both numbers are equal")
+#     else :
+#      print("Second number is greater than first or equal") 
+    
+
+
+# a = 12
+# b = 22
+ 
+# isGreater(a,b)
+# calmean(a,b)                                
+
+# x = 33
+# y = 11
+
+# isGreater(x,y)
+# calmean(x,y)
+
+# K = 56
+# L = 56
+
+# isGreater(K,L)
+# calmean(K,L)
+
+
+
+
+
+
+# def avg(a=10,b=22):
+#     print("The average of two numbers is ",(a+b)/2)
+    
+
+# avg()
+# avg(3,3)
+# avg(33,44)    
+# avg(10)
+# avg(10,0)
+# avg(0,0)
+
+
+
+
+
+# def avg(*num):
+#     sum = 0
+#     for i in num:
+#         sum = sum + i
+#         print("the avg is ", sum/len(num))
+
+
+# avg(32,34)        
+
+
+
+# def avg(*num):
+#     sum = 0
+#     for i in num:
+#         sum = sum + i
+#         return  sum/len(num)
+
+
+# cal = avg(12,33)   
+# print(cal)
+
+
+
+
