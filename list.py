e162402955c0f7937d02478ca17f8e417c64e48b
abdul Razzaq <ar8937562@gmail.com>
2023-12-20 00:44:13 +0500

    List in Python

---
+++ list.py
@@ -0,0 +1,40 @@
+marks = [3,6,9,12,15,18]
+
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+
+# marks.append("add other marks")
+# print(marks)
+
+
+# if 9 in marks:
+#     print("yes")
+# else:
+#     print("No")    
+    
+
+# if 12 in marks:
+#     print("yes")
+# else:
+#     print("No")    
+   
+   
+   
+  # Slicing in List in Python
+  
+# print(marks[1:])   
+# print(marks[1:4])
+# print(marks[1:3:2])
+
+
+
+
+
+# Lst = [i*i for i in range(5)]
+# print(Lst)
+
+# Lst = [i+i for i in range(5)]
+# print(Lst)
