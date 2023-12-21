027b75be98ec702008a47fd1a0d49b9d9c1b315b
abdul Razzaq <ar8937562@gmail.com>
2023-12-21 23:56:31 +0500

    Fibonacci Series in Python

--- recursion.py
+++ recursion.py
@@ -1,11 +1,36 @@
 
                                      # Factorial
-def factorial(n):
-    if(n==0 or n==1):
-     return 1
+# def factorial(n):
+#     if(n==0 or n==1):
+#      return 1
+#     else:
+#         return n * factorial(n-1)
+    
+    
+# print(factorial(4))   
+# print(factorial(0)) 
+
+
+
+                                     # Fibonacci Series
+
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
     else:
-        return n * factorial(n-1)
-    
-    
-print(factorial(4))   
-print(factorial(0)) 
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+def generate_fibonacci_series(n):
+    fib_series = [fibonacci_recursive(i) for i in range(n)]
+    return fib_series
+
+def main():
+    # Get user input for the length of the Fibonacci series
+    n = int(input("Enter the number of terms for the Fibonacci series: "))
+
+    # Generate and print the Fibonacci series
+    result = generate_fibonacci_series(n)
+    print(f"Fibonacci series with {n} terms: {result}")
+
+if __name__ == "__main__":
+    main()                                     
