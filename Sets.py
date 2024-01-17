a8f3b7eddb401d8b969df39f2ad88deb432a5ee4
abdul Razzaq <ar8937562@gmail.com>
2024-01-17 21:26:45 +0500

    Sets in Python

---
+++ Sets.py
@@ -0,0 +1,16 @@
+Fruits = {"mango","banana","apple","orange","banana"}
+animals = {"dog","cat","rat","elephant"}
+# print(Fruits)
+
+# Fruits.add("pine Apple")
+# print(Fruits)
+
+# Fruits.pop()
+# print(Fruits)
+
+# Fruits.remove("apple")
+# print(Fruits)
+
+# print(animals.isdisjoint(Fruits))
+
+
