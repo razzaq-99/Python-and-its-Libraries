0c6648ac13064a80b0f0a50541fc83671807a18c
abdul Razzaq <ar8937562@gmail.com>
2024-01-17 21:49:07 +0500

    Modules in Python

---
+++ Modules.py
@@ -0,0 +1,19 @@
+# import datetime
+
+# x = datetime.datetime.now()
+# print(x)
+
+
+# import platform
+
+# x = platform.system()
+# x = platform.architecture()
+# x = platform.processor()
+# x = platform.machine()
+# x = platform.node()
+
+# print(x)
+
+
+
+# There are also other modules as well like math module or you can make modules manually.
