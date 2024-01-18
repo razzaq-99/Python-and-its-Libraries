815c954197d2f94a2453fa92540bd58ed363f6c5
abdul Razzaq <ar8937562@gmail.com>
2024-01-18 20:48:08 +0500

    Concatenate operations and Split operations in Numpy

--- Numpy.py
+++ Numpy.py
@@ -29,8 +29,8 @@ import numpy as np
 # print(arr5[0:])
 
 
-arr6 = np.array([[1,2,3,4],[5,6,7,8],[7,9,11,0]])
-arr7 = np.array([2])
+# arr6 = np.array([[1,2,3,4],[5,6,7,8],[7,9,11,0]])
+# arr7 = np.array([2])
 # print(arr6[0:2,1:3])
 # print(arr6[:4,:2])
 # print(arr6[:4,:1])
@@ -50,4 +50,25 @@ arr7 = np.array([2])
 
 
 
+
+# arr8 = np.array([[11,22,33],[44,55,66]])
+# arr9 = np.array([[12,23,31],[42,53,61]])
+
+# print(np.concatenate([arr8,arr9]))              # Concatenate
+# print(np.hstack([arr8,arr9]))                   # Horizontal Concatenate
+# print(np.vstack([arr8,arr9]))                     # Vertical Concatenate
+
+
+
+arr = np.array([12,34,55,66,12,22,44,77])
+
+# print(np.array_split(arr,2))
+# print(np.array_split(arr,3))
+# print(np.array_split(arr,4))
+# print(np.array_split(arr,5))
+# print(np.array_split(arr,8))
+
+
+arr0 = np.array_split(arr,4)
+print(arr0[1])
 
