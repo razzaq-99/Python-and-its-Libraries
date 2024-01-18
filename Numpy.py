b8befbd465ada748f32c393663b6b925e705b889
abdul Razzaq <ar8937562@gmail.com>
2024-01-18 17:31:29 +0500

    All about Python library Numpy

---
+++ Numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# l1 = [10,20,30]
+# l2 = [40,50,60]
+
+# print(l1+l2)
+
+
+# arr1 = np.array([10,20,30])
+# arr2 = np.array([40,50,60])
+
+
+# print(arr1)
+# print(arr2)
+# print(arr1+arr2)
+# print(arr1*arr2)
+
+# arr3 = np.array([11,22,"33",44.0])
+# print(arr3)
