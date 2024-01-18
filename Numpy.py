3f97c64ca1cc89071cd663c6e2766d079a9b6c04
abdul Razzaq <ar8937562@gmail.com>
2024-01-18 20:30:48 +0500

    Slicing and other operations in Numpy

--- Numpy.py
+++ Numpy.py
@@ -17,3 +17,37 @@ import numpy as np
 
 # arr3 = np.array([11,22,"33",44.0])
 # print(arr3)
+
+# arr4 = np.array([[1,2,3,4],[5,6,7,8]])
+
+# print(arr4)
+
+# arr5 = np.array([12,33,44,55,22])    #Slicing
+
+# print(arr5[1:3])
+# print(arr5[:2])
+# print(arr5[0:])
+
+
+arr6 = np.array([[1,2,3,4],[5,6,7,8],[7,9,11,0]])
+arr7 = np.array([2])
+# print(arr6[0:2,1:3])
+# print(arr6[:4,:2])
+# print(arr6[:4,:1])
+
+# print(arr6[2,2])
+# print(arr6[1,1])
+# print(arr6[0,0])
+
+# print(np.sum(arr6))
+# print(np.shape(arr6))
+# print(np.max(arr6))
+# print(np.size(arr6))
+
+
+# print(np.power(arr6,arr7))
+# print(np.sqrt(arr6))
+
+
+
+
