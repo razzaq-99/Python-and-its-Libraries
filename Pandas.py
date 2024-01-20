cf64917d5cdc3a63a1d9e71fdecfd043dd5692ab
abdul Razzaq <ar8937562@gmail.com>
2024-01-20 16:54:40 +0500

    Exploring data from csv files in Pandas Python

--- Pandas.py
+++ Pandas.py
@@ -1,9 +1,23 @@
 import pandas as pd
 
-data = { "Name":['Razzaq','Ali','Ahmed'],
-         "Age":[20,21,22],
-         "Salary":[500000,200000,100000]
-         }
+# data = { "Name":['Razzaq','Ali','Ahmed'],
+#          "Age":[20,21,22],
+#          "Salary":[500000,200000,100000]
+#          }
 
-df = pd.DataFrame(data)
-print(df)
+# df = pd.DataFrame(data)
+# print(df)
+
+
+
+
+
+
+data = pd.read_csv("Hotel Reservations.csv")
+
+# print(data)
+# print(data.head(10))
+# print(data.tail(10))
+# print(data.info())
+# print(data.describe())
+print(data.isnull().sum())
