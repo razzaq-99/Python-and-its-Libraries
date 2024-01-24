9257c607dbd44ab49c5ce78ca61459d409fc37ed
abdul Razzaq <ar8937562@gmail.com>
2024-01-24 22:17:42 +0500

    Introduction to line plot and its properties

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,5 +1,5 @@
 
-                                                                 # Bar plot
+                                                                 # BAR PLOT
 
 # import pandas as pd
 # import matplotlib.pyplot as plt
@@ -19,25 +19,61 @@
 # plt.show()
 
 
-import pandas as pd
-import matplotlib.pyplot as plt
-data = pd.read_excel("expense3.xlsx")
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# data = pd.read_excel("expense3.xlsx")
 
 
+
+# df = pd.DataFrame(data)
 # print(df)
-df = pd.DataFrame(data)
-df["Payment Mode"] = df["Payment Mode"].astype(str)
+
+# df["Payment Mode"] = df["Payment Mode"].astype(str)
 # plt.bar(df["Payment Mode"],df["Amount"])
 # plt.show()
 
 
 
-dfx = df.groupby("Payment Mode")["Amount"].sum()
+# dfx = df.groupby("Payment Mode")["Amount"].sum()
+# print(df)
+
+# plt.xlabel("Payment Methods",fontsize = 17)
+# plt.ylabel("Amount",fontsize = 17)
+# plt.title("Payment Methods vs Amount",fontsize = 20)
+# colors = ["red","green","orange","blue"]
+# plt.bar(dfx.index,dfx.values,color = colors)
+# plt.show()
+
+
+# df["Category"] = df["Category"].astype(str)
+
+# dfx = df.groupby("Category")["Amount"].sum()
 # print(dfx)
 
-plt.xlabel("Payment Methods",fontsize = 17)
-plt.ylabel("Amount",fontsize = 17)
-plt.title("Payment Methods vs Amount",fontsize = 20)
 
-plt.bar(dfx.index,dfx.values)
+# plt.xlabel("Category")
+# plt.ylabel("Amount")
+# plt.title("Category vs Amount")
+# plt.bar(df["Category"],df["Amount"])
+# plt.bar(dfx.index,dfx.values)
+# plt.show()
+
+
+
+
+                                                               # LINE PLOT
+                                                               
+import matplotlib.pyplot as plt
+x = ["day1","day2","day3","day4","day5","day6"]
+y = [100,400,120,330,20,200]
+z = [200,500,40,250,0,400]
+z1 = [400,600,350,100,50,30]
+
+
+# plt.plot(x,y)
+# plt.plot(x,y,marker="*")
+plt.plot(x,y,marker = "^",ls ="--",color="red",label="week1")
+plt.plot(x,z,marker = "^",ls ="--",color="blue",label="week2")
+plt.plot(x,z1,marker = "^",ls ="-",color="green",label="week3")
+plt.legend()
 plt.show()
