5c34c7c3fc124a1fa4ade52f5fe5526cae993be4
abdul Razzaq <ar8937562@gmail.com>
2024-01-24 14:44:10 +0500

    Bar chart and its properties in Matplotlib

--- Matplotlib.py
+++ Matplotlib.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 
 y = [99,70,97,66,23,44]
 x = ["Part1","Part2","Part3","Part4","Part5","Part6"]
+colors = ["red","green","orange","blue","yellow","red"]
 
-plt.bar(x,y)
+
+
+# plt.bar(x,y,color = "orange")
+# plt.bar(x,y,color = colors,)
+plt.bar(x,y,color = colors,edgecolor="black")
+plt.xlabel("Parts of Harry Potter ",fontsize = 18)
+plt.ylabel("Popularity",fontsize = 18)
+plt.title("Popularity of Harry Potter",fontsize = 18)
 plt.show()
